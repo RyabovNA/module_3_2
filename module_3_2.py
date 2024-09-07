3d6568f9fcd07035aeab93c2ea03f8d682c7a992
RyabovNA <kulek19862009@mail.ru>
2024-09-07 10:54:23 +0500

    Add files via upload

--- module_3_2.py
+++ module_3_2.py
@@ -1,6 +1,10 @@
 def send_email(message, recipient, *, sender = "university.help@gmail.com"):
-    if "@" and (".com" or ".ru" or ".net") not in (recipient and sender):
+    domain = [".com", ".ru", ".net"]
+
+    if "@" not in recipient or "@" not in sender \
+            or not recipient.endswith((".com", ".ru", ".net")) or not sender.endswith((".com", ".ru", ".net")):
         print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
+
     elif recipient == sender:
         print("Нельзя отправить письмо самому себе!")
     elif sender == "university.help@gmail.com":
